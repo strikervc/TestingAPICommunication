dfbcae2da013d0243d3f448a8646d8e1b3952579
Victor Valdez <victor334_@hotmail.com>
2021-03-19 23:05:30 -0400

    initial commit

---
+++ TestingAPICommunication/Models/Definition.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace TestingAPICommunication.Models
+{
+    public class Definition
+    {
+        public string Word { get; set; }
+        public ObservableCollection<Item> Definitions { get; set; }
+        
+    }
+}
